1677dee6d11b1bb2858ed5e0bb112942d43c3632
Pragadeeswaran S <pragadeeswaran.s@MacBook-Pro-7.local>
2024-06-09 22:13:58 +0530

    first commit contains tf script for ec2 creation

---
+++ main.tf
@@ -0,0 +1,65 @@
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create Subnet
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Create Security Group
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "instance" {
+  ami                         = "ami-0c55b159cbfafe1f0"  
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.main.id
+  security_groups             = [aws_security_group.main.name]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Terraform-Instance"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "The region to deploy the EC2 instance in"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of the EC2 instance"
+  type        = string
+}
